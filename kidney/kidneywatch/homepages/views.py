67a85f44097765ab4033ef285e6cd10e17e07a59
Nick Harris <94771240+nh299@users.noreply.github.com>
2022-11-28 16:54:49 -0700

    Beginning of the site

---
+++ kidney/kidneywatch/homepages/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def indexPageView(request):
+    return render(request, 'index.html')
+
+def infoPageView(request):
+    return render(request, 'info.html')
+
+def dashboardPageView(request):
+    return render(request, 'dashboard.html')
