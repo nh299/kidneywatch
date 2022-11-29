e5272f10bf9ebcc69c7b2044ddbcff44fbfa056a
Nick Harris <94771240+nh299@users.noreply.github.com>
2022-11-29 00:12:42 -0700

    search bar

--- kidney/kidneywatch/homepages/urls.py
+++ kidney/kidneywatch/homepages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import indexPageView, dashboardPageView, infoPageView, testPageView, dataRender
+from .views import indexPageView, dashboardPageView, infoPageView, dataRender
 
 urlpatterns = [
     path("", indexPageView, name="index"),
     path("dashboard", dashboardPageView, name='dashboard'),
     path("info", infoPageView, name='info'),
-    path("test", dataRender, name='test')
+    path("test", dataRender, name='test'),
 ]
--- kidney/kidneywatch/homepages/views.py
+++ kidney/kidneywatch/homepages/views.py
@@ -18,20 +18,23 @@ def testPageView(request):
     return render(request, 'test.html')
 
 def dataRender(request):
+    search = request.POST.get('searchFood')
     parameters = { 
         "api_key": 'EPMl3IkB2Wb9GzdAbcfaaYkCCucSG7JQxbGUoWGK',
-        "query": 'Cheese',
+        "query": search,
         "dataType": ["Foundation"]
     }
+    if search != None:
+        response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/search", params=parameters)
 
-    response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/search", params=parameters)
-
-    data = response.json()
-    foods=[]
-    for f in range(len(data['foods'])):
-        food = data["foods"][f]['description']
-        foods.append(food)
-    context = {
-        'foods':foods
-    }
-    return render(request, 'test.html', context)
+        data = response.json()
+        foods=[]
+        for f in range(len(data['foods'])):
+            food = data["foods"][f]['description']
+            foods.append(food)
+        context = {
+            'foods':foods
+        }
+        return render(request, 'test.html', context)
+    else:
+        return render(request, 'test.html')
