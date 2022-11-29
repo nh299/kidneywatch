97a5d031af2cd5822d6d7b2dc8190efbeed5ef1f
Nick Harris <94771240+nh299@users.noreply.github.com>
2022-11-28 18:51:25 -0700

    changes

--- kidney/kidneywatch/homepages/urls.py
+++ kidney/kidneywatch/homepages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import indexPageView
+from .views import dashboardPageView
 
 urlpatterns = [
-    path("", indexPageView, name="index")
+    path("", indexPageView, name="index"),
+    path("/dashboard", dashboardPageView, name='dashboard')
 ]
